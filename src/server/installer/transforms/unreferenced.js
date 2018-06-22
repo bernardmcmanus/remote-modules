a94d22cf836adfa4092379e1b2bfd4fdf8736f7d
Bernard McManus <bernard.mcmanus.iv@gmail.com>
2018-06-21 21:33:25 -0400

    transform fixes

--- src/server/installer/transforms/unreferenced.js
+++ src/server/installer/transforms/unreferenced.js
@@ -1,6 +1,9 @@
+import * as t from '@babel/types';
 import generate from '@babel/generator';
 import traverse from '@babel/traverse';
 import { codeFrameColumns } from '@babel/code-frame';
+
+import { isPrimitive } from '../../../lib/helpers';
 
 function containsReferences(path, nodes) {
 	let result = false;
@@ -20,7 +23,7 @@ function containsReferences(path, nodes) {
 	return result;
 }
 
-function generateRemovalFrame(path) {
+function generateRemovalFrame(path, reason) {
 	const { code } = generate(path.node);
 	const lineCount = code.split('\n').length;
 	const location = {
@@ -30,7 +33,7 @@ function generateRemovalFrame(path) {
 	return codeFrameColumns(code, location, {
 		highlightCode: true,
 		linesAbove: lineCount,
-		message: `Removing unreferenced ${path.node.type}`
+		message: `Removing ${reason} ${path.node.type}`
 	});
 }
 
@@ -39,13 +42,77 @@ export default (api, { logger, moduleId }) => ({
 		Scope: {
 			exit(outerPath, state) {
 				/**
-				 * Avoid clearing cache / crawling more than necessary.
-				 * babel-plugin-minify-dead-code-elimination will have already done this.
+				 * Avoid clearing cache more than necessary.
+				 * minify-dead-code-elimination will have already done this.
 				 */
 				if (!state.file.opts.plugins.some(({ key }) => key === 'minify-dead-code-elimination')) {
 					traverse.cache.clear();
-					outerPath.scope.crawl();
 				}
+
+				outerPath.scope.crawl();
+
+				outerPath.traverse({
+					ConditionalExpression: {
+						exit(innerPath) {
+							const { confident, value } = innerPath.get('test').evaluate();
+							if (confident) {
+								const { consequent, alternate } = innerPath.node;
+								innerPath.replaceWith(value ? consequent : alternate);
+								innerPath.scope.crawl();
+							}
+						}
+					},
+					IfStatement: {
+						exit(innerPath) {
+							const { confident, value } = innerPath.get('test').evaluate();
+							if (confident && !value) {
+								logger.trace(`${moduleId}:\n\n${generateRemovalFrame(innerPath, 'falsy')}\n`);
+								innerPath.remove();
+								innerPath.scope.crawl();
+							}
+						}
+					},
+					VariableDeclarator: {
+						exit(innerPath) {
+							if (!innerPath.node.init) {
+								const binding = innerPath.scope.getBinding(innerPath.node.id.name);
+								if (binding && binding.constant && !t.isLoop(innerPath.scope.block)) {
+									// eslint-disable-next-line no-param-reassign
+									innerPath.node.init = t.Identifier('undefined');
+								}
+							}
+						}
+					},
+					ReferencedIdentifier(innerPath) {
+						if (innerPath.scope.getBinding(innerPath.node.name)) {
+							const { confident, value } = innerPath.evaluate();
+							if (confident && isPrimitive(value)) {
+								let replacementNode;
+								switch (true) {
+									case typeof value === 'string':
+										replacementNode = t.StringLiteral(value);
+										break;
+									case typeof value === 'number':
+										replacementNode = t.NumericLiteral(value);
+										break;
+									case typeof value === 'boolean':
+										replacementNode = t.BooleanLiteral(value);
+										break;
+									case value === null:
+									case value === undefined:
+										replacementNode = t.Identifier(String(value));
+										break;
+									default:
+										// noop
+										break;
+								}
+								if (replacementNode) {
+									innerPath.replaceWith(replacementNode);
+								}
+							}
+						}
+					}
+				});
 
 				(function removeUnreferenced() {
 					const possiblyUnreferenced = new Map();
@@ -87,11 +154,25 @@ export default (api, { logger, moduleId }) => ({
 										possiblyUnreferenced.delete(innerPath.node.name);
 									}
 								}
+							},
+							FunctionDeclaration(innerPath) {
+								const { name } = innerPath.node.id;
+								if (possiblyUnreferenced.has(name)) {
+									// Don't remove identifiers that are referenced in child scopes
+									if (!innerPath.parentPath.scope.hasOwnBinding(name)) {
+										possiblyUnreferenced.delete(name);
+									} else {
+										// This path is going to be removed so we don't care about traversing any further
+										innerPath.skip();
+									}
+								}
 							}
 						});
 
 						possiblyUnreferenced.forEach(bindingPath => {
-							logger.trace(`${moduleId}:\n\n${generateRemovalFrame(bindingPath)}\n`);
+							logger.trace(
+								`${moduleId}:\n\n${generateRemovalFrame(bindingPath, 'unreferenced')}\n`
+							);
 							bindingPath.remove();
 						});
 
